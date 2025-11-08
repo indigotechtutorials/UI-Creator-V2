0af77cafd7ba49d5be568d1f757cfcb329f29692
Enoch Tamulonis <enochtamulonis@gmail.com>
2025-11-08 10:39:51 +0700

    add outline btn

--- app/javascript/controllers/color_picker_controller.js
+++ app/javascript/controllers/color_picker_controller.js
@@ -3,14 +3,5 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="color-picker"
 export default class extends Controller {
   connect() {
-    let input = this.element.querySelector("input")
-    Coloris({
-    themeMode: 'auto',
-    format: 'hsl',
-    onChange: (color, inputEl) => {
-      console.log(`The new color is ${color}`);
-    },
-    el: this.element,
-  });
   }
 }
