a3c70d972f4793707eb73933cdc985e98a196cb8
Enoch Tamulonis <enochtamulonis@gmail.com>
2025-11-11 14:48:41 +0700

    moves color picker preview into component using view component and then add in select for secondary color

---
+++ app/components/color_scheme_preview_component.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class ColorSchemePreviewComponent < ViewComponent::Base
+  attr_reader :name, :color
+  def initialize(name, color)
+    @name = name
+    @color = color
+  end
+end
