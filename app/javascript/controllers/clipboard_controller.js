c06d5cc736072e6b325c8e4b1811eea8b0940686
Enoch Tamulonis <enochtamulonis@gmail.com>
2025-11-11 15:23:39 +0700

    rm clipboard toggle ui code.

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -3,16 +3,11 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="clipboard"
 export default class extends Controller {
   static values = { text: String }
-  static targets = ["success"]
 
   copy(e) {
     e.preventDefault()
     navigator.clipboard.writeText(this.textValue).then(() => {
-      console.log('Async: Copying to clipboard was successful!');
-      this.successTarget.style.display = "flex"
-      setTimeout(() => {
-        this.successTarget.style.display = "none"
-      }, 1000)
+      
     }, (err) => {
       console.error('Async: Could not copy text: ', err);
     });
